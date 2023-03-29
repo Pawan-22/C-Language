25e424483bb295ffe0de66387060d2df12eb1ea3
Pawan <pawanptl34@gmail.com>
2023-03-29 17:54:38 +0530

    Counts the digit

---
+++ Loops/DigitCount.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, count = 0, temp;
+    printf("Enter Number\n");
+    scanf("%d", &n);
+
+    // This counts number of digits in a given number.
+
+    while (n > 0)
+    {
+        n /= 10;
+        count += 1;
+    }
+
+    printf("Digits in given number are %d\n", count);
+    return 0;
+}
