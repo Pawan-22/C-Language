773396efc778760d2c1d88f886a0ce10d2c2c864
Pawan <pawanptl34@gmail.com>
2023-04-28 13:38:02 +0530

    arrays basic

---
+++ Arrays/arrays.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int marks[4];
+    marks[0] = 90;
+    marks[1] = 80;
+    marks[2] = 70;
+    marks[3] = 60;
+
+for(int i = 0; i < 4; i++){
+    printf("%d\n", marks[i]);
+    }
+}
